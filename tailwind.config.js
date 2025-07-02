d5fd478e083d4175a34d8ce771d218b4b24d3702
Graciele Miki Sazaka <gracielesazaka@gmail.com>
2025-07-02 15:51:56 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    colors: {
+      brown: {
+        light: '#ce7e5d',
+        dark: '#a2543d',
+      },
+      beige: {
+        light: '#f7d1ba',
+      }
+    }
+  }
+}
