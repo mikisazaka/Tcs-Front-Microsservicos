62caa48b94fced9586834ea2874499ab2e7ce020
Graciele Miki Sazaka <gracielesazaka@gmail.com>
2025-07-07 00:13:24 -0300

    feat: Alteração de menubar e navbar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { CommonModule } from '@angular/common';
-import { MenubarComponent } from './menubar/menubar.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LivrosPopularesComponent } from './livros-populares/livros-populares.component';
 
 @NgModule({
-  declarations: [MenubarComponent, NavbarComponent, LivrosPopularesComponent],
+  declarations: [NavbarComponent, LivrosPopularesComponent],
   imports: [CommonModule, MatButtonModule, MatSidenavModule],
-  exports: [MenubarComponent, NavbarComponent, LivrosPopularesComponent, CommonModule, MatButtonModule, MatSidenavModule], 
+  exports: [NavbarComponent, LivrosPopularesComponent, CommonModule, MatButtonModule, MatSidenavModule], 
 })
 export class SharedModule {}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,16 @@
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
-    "./node_modules/flowbite/**/*.js"
+    "./node_modules/flowbite/**/*.js",
+    "./pages/**/*.{js,ts,jsx,tsx}", 
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
       colors: {
-        brown: {
-          light: '#ce7e5d',
-          dark: '#a2543d',
-        },
-        beige: {
-          light: '#f7d1ba',
-        }
+        'brown-light': '#ce7e5d',
+        'brown-dark': '#a2543d',
+        'beige-light': '#f7d1ba'
       }
     }
   },
