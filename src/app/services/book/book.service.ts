794e8ddde18017d1eddbcb123b570318238724a8
Leandro Hideki <htwleandro@gmail.com>
2025-07-12 23:40:31 -0300

    Update book.service.ts

--- src/app/services/book/book.service.ts
+++ src/app/services/book/book.service.ts
@@ -49,20 +49,4 @@ export class BookService {
   encontrarPorId(id: number): Observable<Book> {
     return this.http.get<Book>(`${this.API_URL}/${id}`);
   }
-
-  getLivros(): Observable<Book[]> {
-    return this.http.get<Book[]>(`${this.API_URL}`);
-  }
-
-  removerLivro(id: number): Observable<Book> {
-    return this.http.delete<Book>(`${this.API_URL}/remove/${id}`);
-  }
-
-  editarLivro(id: number, book: Book): Observable<Book> {
-    return this.http.put<Book>(`${this.API_URL}/edit/${id}`, book);
-  }
-
-  encontrarPorId(id: number): Observable<Book> {
-    return this.http.get<Book>(`${this.API_URL}/${id}`);
-  }
 }
