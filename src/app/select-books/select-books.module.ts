34b381804097d4ec90adcba3bacd08c0b96e87a1
Graciele Miki Sazaka <gracielesazaka@gmail.com>
2025-07-14 00:28:53 -0300

    feat: Filtragem e alteração de botões

--- src/app/select-books/select-books.module.ts
+++ src/app/select-books/select-books.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { SelectBooksComponent } from './select-books.component';
 import { SharedModule } from 'app/shared/shared.module';
 import { SelectBooksRoutingModule } from './select-books-routing.module';
 
 @NgModule({
   declarations: [SelectBooksComponent],
-  imports: [CommonModule, SharedModule, SelectBooksRoutingModule],
+  imports: [CommonModule, FormsModule, SharedModule, SelectBooksRoutingModule],
   exports: [SelectBooksComponent]
 })
 export class SelectBooksModule {}
