2cb005022ca056cfb144452a2710bdb4749b8b51
Leandro Hideki <htwleandro@gmail.com>
2025-07-16 15:49:55 -0300

    Add files via upload

--- src/app/models/review.model.ts
+++ src/app/models/review.model.ts
@@ -1,7 +1,12 @@
+import { Book } from "./bookDetail.model"
+
 export interface Review {
     userId: number
     bookId: number
     rating: number
     title: string
     comment: string
+    username: string
+    createdAt: string
+    book?: Book
 }
