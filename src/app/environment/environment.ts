96e7290603b3adc36f894aabb94cd712404a808f
Leandro Hideki <htwleandro@gmail.com>
2025-07-18 12:27:52 -0300

    Update environment.ts

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -2,9 +2,8 @@ export const environment = {
     production: false,
     geminiApiKey: 'AIzaSyB-Br575iFb3r0NUGLDyfHb4DnR_DwrHcA',
     backendApiUrl: [
-      "localhost:8887",
-      "localhost:8888",
-      "localhost:8889",
-      "localhost:4200"
+      "http://localhost:8887",
+      "http://localhost:8888",
+      "http://localhost:8889",
     ]
   };
