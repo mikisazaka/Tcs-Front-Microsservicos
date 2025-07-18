985dbb9329af2da3ad273550e2b78c2be4a56c8b
Libonico <pedrorochaliboni@hotmail.com>
2025-07-18 11:18:04 -0300

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
+import { ChatbotComponent } from './chatbot/chatbot.component';
+import { SharedModule } from './shared/shared.module'; 
 
 @Component({
   selector: 'app-root',
-  standalone: false,
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    ChatbotComponent,
+    SharedModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
   protected title = 'tcs-front-microsservicos';
