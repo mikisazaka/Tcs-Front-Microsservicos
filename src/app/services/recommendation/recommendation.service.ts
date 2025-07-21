8ef2c7f96e4ec374526c480899cb535c416803b0
Graciele Miki Sazaka <gracielesazaka@gmail.com>
2025-07-21 15:53:19 -0300

    Add files via upload

---
+++ src/app/services/recommendation/recommendation.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AuthService } from 'app/auth/auth.service';
+import { Book } from 'app/models/bookDetail.model';
+import { Observable, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecommendationService {
+
+  private API_URL = 'http://localhost:8886/recommendations';
+
+  constructor(private http: HttpClient, public authService: AuthService) { }
+
+  getRecomendacoes(): Observable<Book[]> {
+    const userId = this.authService.getUserId();
+    return this.http.get<Book[]>(`${this.API_URL}/${userId}`);
+  }
+}
