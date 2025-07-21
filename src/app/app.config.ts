ef2e8e65cf3682067838e37e97e8a9dac1ddaa4b
Graciele Miki Sazaka <gracielesazaka@gmail.com>
2025-07-21 15:51:19 -0300

    Add files via upload

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app-routing.module';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tokenInterceptor } from './auth/token.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(
+      withInterceptors([tokenInterceptor])
+    )
   ]
 };
